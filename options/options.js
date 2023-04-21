2dcb487fde075e06250023b821073bcc3853ee25
Crowdbotics <team@crowdbotics.com>
2023-04-21 14:34:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "figo-40093",
+  url: "https://figo-40093.botics.co",
+  api: "https://figo-40093.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
